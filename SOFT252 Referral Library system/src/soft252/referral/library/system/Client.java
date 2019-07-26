bb0de6040cc85188799ac99f52b6cf83a5dd4617
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-07-26 18:17:00 +0100

    started adding classes

---
+++ SOFT252 Referral Library system/src/soft252/referral/library/system/Client.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soft252.referral.library.system;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Gearing
+ */
+public class Client {
+    protected String ID;
+    protected String password;
+    
+    List<Object> resourcesBorrowed = new ArrayList<Object>();
+    
+    
+}
