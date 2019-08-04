87c8946f3fe52ff4b11b94c71c41e29e10759aac
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-08-04 17:49:52 +0100

    set up resource class

---
+++ SOFT252 Referral Library system/src/soft252/referral/library/system/resources.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soft252.referral.library.system;
+
+/**
+ *
+ * @author Gearing
+ */
+public class resources {
+    protected String catagorey;
+    protected String resourceID;
+    protected String title;
+    protected String resourceType;
+    
+    protected boolean borrowed;
+    protected boolean borrowedLong;
+    protected int daysBorrowed = 0;
+    protected int rating = 0;
+    
+    
+    resources(String catagorey, String ID, String title, String type){
+        this.catagorey = catagorey;
+        this.resourceID = ID;
+        this.title = title;
+        this.resourceType = type;
+    }
+}
